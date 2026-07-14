4212ccf4d1db424836881405df379575692828fb
Suriya G <suriyanarayanang16@gmail.com>
2026-07-14 15:56:11 +0530

    Blogs and Locations

--- src/db/index.ts
+++ src/db/index.ts
@@ -15,7 +15,7 @@ let _db: ReturnType<typeof getDb> | undefined;
 export const db = new Proxy({} as ReturnType<typeof getDb>, {
   get(_target, prop) {
     if (!_db) _db = getDb();
-    return (_db as Record<string | symbol, unknown>)[prop];
+    return (_db as unknown as Record<string | symbol, unknown>)[prop];
   },
 });
 
