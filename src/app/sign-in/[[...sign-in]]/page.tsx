9ae713fff160562979f87840cdf8a3aea50ab9eb
Suriya G <suriyanarayanang16@gmail.com>
2026-07-29 15:41:54 +0530

    Fix Clerk routing

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex h-screen w-full items-center justify-center">
-      <SignIn />
+      <SignIn routing="path" path="/sign-in" />
     </div>
   );
 }
